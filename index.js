899cc201b650b3c763369208dd40e803e8d30d4b
mohsen-PC\mohsen <mhsn.govahi@gmail.com>
2019-10-23 21:35:22 +0330

    bundler options added.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const args = process.argv.slice(2);
 var config_data = require("./"+args[0]);
 
 staticHtmlBundler.bundle({
-	tags: config_data.tags,
-	inputHtmlPath: config_data.inputHtml,
-	outputHtmlPath: config_data.outputHtml,
 	sourceFolder: config_data.sourceFolder,
 	distFolder: config_data.distFolder,
+	inputHtmlPath: config_data.inputHtml,
+	outputHtmlPath: config_data.outputHtml,
 	distCssFolder: config_data.cssFolder,
-	distJsFolder: config_data.jsFolder
+	distJsFolder: config_data.jsFolder,
+	tags: config_data.tags
 });
